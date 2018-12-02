97ce0ada5693aadff895a3b4ac50be7c1473b077
Ivan Smirnov <i.s.smirnov@gmail.com>
2018-12-02 16:36:44 +0000

    2018 - day 02

---
+++ src/day02.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+
+pub fn day02_1(input: &str) -> u32 {
+    let mut map = HashMap::new();
+    let (a, b) = input
+        .trim()
+        .split_whitespace()
+        .map(|id| {
+            map.clear();
+            id.chars().for_each(|c| *map.entry(c).or_default() += 1);
+            let (a, b) = map.values().fold((false, false), |acc, x| {
+                (acc.0 || x % 2 == 0, acc.1 || x % 3 == 0)
+            });
+            (a as u32, b as u32)
+        })
+        .fold((0, 0), |acc, (a, b)| (acc.0 + a, acc.1 + b));
+    a * b
+}
+
+pub fn day02_2(input: &str) -> String {
+    let input = input.trim();
+    for id1 in input.split_whitespace() {
+        for id2 in input.split_whitespace() {
+            let (i, n) =
+                id1.chars()
+                    .zip(id2.chars())
+                    .enumerate()
+                    .fold((0, 0), |acc, (i, (c1, c2))| {
+                        let neq = c1 != c2;
+                        (if neq { i } else { acc.0 }, acc.1 + neq as u32)
+                    });
+            if n == 1 {
+                return format!("{}{}", &id1[..i], &id1[(i + 1)..]);
+            }
+        }
+    }
+    Default::default()
+}
+
+#[test]
+fn test_day01_1() {
+    const SAMPLE: &str = "abcdef bababc abbcde abcccd aabcdd abcdee ababab";
+    assert_eq!(day02_1(SAMPLE), 12);
+    const INPUT: &str = include_str!("../input/02.txt");
+    assert_eq!(day02_1(INPUT), 6175);
+}
+
+#[test]
+fn test_day02_1() {
+    const SAMPLE: &str = "abcde fghij klmno pqrst fguij axcye wvxyz";
+    assert_eq!(day02_2(SAMPLE), "fgij");
+    const INPUT: &str = include_str!("../input/02.txt");
+    assert_eq!(day02_2(INPUT), "asgwjcmzredihqoutcylvzinx");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod util;
 
 pub mod day01;
+pub mod day02;
